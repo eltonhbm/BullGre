3ff8bc58623aec95a7e7cdfd0c6c4d0ca2cc403e
Elton Monteiro <elton@sistemasbr.net>
2022-12-06 10:40:24 -0300

    Listagem e cadastro de fazendas

---
+++ BullGre/Migrations/20221206124953_Tabela Fazenda.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BullGre.Migrations
+{
+    /// <inheritdoc />
+    public partial class TabelaFazenda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fazenda",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Cidade = table.Column<string>(type: "TEXT", nullable: false),
+                    Hectares = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fazenda", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fazenda");
+        }
+    }
+}
